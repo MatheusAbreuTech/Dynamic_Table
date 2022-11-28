8123452a88c3bcbbf7fceda49e4c528e1692c39c
MatheusAbreuTech <matheusabreutech@gmail.com>
2022-11-28 11:33:07 -0300

    first commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,45 @@
-import './table.css'
+import "./table.css";
 
 const Table = ({ data, column }) => {
   return (
     <table>
       <thead>
         <tr>
-          {column.map((item, index) => <TableHeadItem item={item} />)}
+          {column.map((item, index) => (
+            <TableHeadItem key={index} item={item} />
+          ))}
         </tr>
       </thead>
       <tbody>
-        {data.map((item, index) => <TableRow item={item} column={column} />)}
+        {data.map((item, index) => (
+          <TableRow key={index} item={item} column={column} />
+        ))}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
-const TableHeadItem = ({ item }) => <th>{item.heading}</th>
+const TableHeadItem = ({ item }) => <th>{item.heading}</th>;
 const TableRow = ({ item, column }) => (
   <tr>
     {column.map((columnItem, index) => {
+      if (columnItem.value.includes(".")) {
+        const itemSplit = columnItem.value.split(".");
+        const tableItem = itemSplit.reduce((prev, next, index) => {
+          if (index === 0) {
+            prev = item[next];
+          } else {
+            prev = prev[next];
+          }
+          return prev;
+        }, "");
 
-      if(columnItem.value.includes('.')) {
-        const itemSplit = columnItem.value.split('.') //['address', 'city']
-        return <td>{item[itemSplit[0]][itemSplit[1]]}</td>
+        return <td key={index}>{tableItem}</td>;
       }
 
-      return <td>{item[`${columnItem.value}`]}</td>
+      return <td key={index}>{item[`${columnItem.value}`]}</td>;
     })}
   </tr>
-)
+);
 
-export default Table
+export default Table;
